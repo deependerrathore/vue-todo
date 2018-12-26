6b08dd90088f852597f04bef454aba33c28cb88b
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-26 13:32:19 +1100

    adding new item to the todo

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input type="text" class="todo-input" v-model="newTodo" placeholder="What needs to be done?"/>
+        <input type="text" class="todo-input" v-model="newTodo" v-on:keyup.enter="addTodo" placeholder="What needs to be done?"/>
         <div v-for="todo in todos" :key="todo.id" class="todo-item">
             {{todo.title}}
         </div>
@@ -19,6 +19,19 @@ export default {
 
       ]
     }
+  },
+  methods:{
+      
+      addTodo:function(){
+        if(this.newTodo.trim().length == 0){
+            return
+        }
+          this.todos.push({
+              'id':this.idForTodo,'title':this.newTodo,'completed':false
+          });
+          this.newTodo='';
+          this.idForTodo++;
+      }
   }
 }
 </script>
