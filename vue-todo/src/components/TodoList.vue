5142d81976d47d07a2a447a940903bc7698e8e62
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-26 13:11:56 +1100

    displaying list of todo-items

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>Todo list will go here</div>
+    <div>
+        <input type="text" class="todo-input" v-model="newTodo" placeholder="What needs to be done?"/>
+        <div v-for="todo in todos" :key="todo.id" class="todo-item">
+            {{todo.title}}
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,12 +12,30 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      todos:[
+            {'id':1,'title':'Finish Vue Screencast','completed':false},
+            {'id':2,'title':'Anhilation','completed':false}
+
+      ]
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+.todo-input{
+    width: 100%;
+    padding: 10px 18px;
+    font-size:18px;
+    margin-bottom: 16px;
 
+    &:focus{
+        outline: 0;
+    }
+}
+.todo-item{
+    text-align: left;
+}
 </style>
+
